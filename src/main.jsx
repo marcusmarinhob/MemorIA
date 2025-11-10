7a17b4e8f2f8bdd2080f2a96a2d4b40bdae13839
anacarvlima <anacecilialima285@gmail.com>
2025-11-10 17:33:23 -0300

    ainda com bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Toaster } from '@/components/ui/toaster'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@/App';
+import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Toaster />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
